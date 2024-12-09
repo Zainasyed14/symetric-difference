21187009de6f9680ec8c13b43194cc0a63d51313
Zainasyed14 <zainasyed12345@gmail.com>
2024-12-09 18:24:43 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+set1 = {'blue' , 'green'}
+set2 = {'blue' , 'yellow'}
+print(set1)
+print(set2)
+print(set1.symmetric_difference(set2))
+
+Set1 = {1,2,3,4,5}
+Set2 = {1,5,6,7,8,9}
+print(Set1)
+print(Set2)
+print(Set1.symmetric_difference(Set2))
